f67b42f1b89250fbf6f36a387c9cb0155d0d7ef3
Ian Anderson <Ian.Michael.Anderson@outlook.com>
2024-11-16 20:47:08 -0600

    Add files via upload

--- voltron-season-9/button_game.gd
+++ voltron-season-9/button_game.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 var won = false
 
-
-
 func _on_button_pressed() -> void:
 	won = true
+	
+	get_node("Background").visible = false
+	get_node("Win").visible = true
+
+func reset():
+	won = false
+	get_node("Background").visible = true
+	get_node("Win").visible = false
